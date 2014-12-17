db09299442e53a086dbb9b4dd02a7f2d90052a07
ruchiraw <ruchira.wageesha@gmail.com>
2014-12-17 23:40:31 +0530

    updating

---
+++ index.js
@@ -0,0 +1,40 @@
+var debug = require('debug')('menu-service');
+var utils = require('utils');
+
+var express = require('express');
+var app = module.exports = express();
+
+app.use(express.json());
+
+app.get('/menus/:id', function (req, res) {
+    //var menu = menus[req.params.id];
+    var menus = [
+        {
+            home: {url: '/', title: 'serandives.com'},
+            menu: [
+                {url: 'https://auto.serandives.com', title: 'Auto'},
+                {url: 'https://hotels.serandives.com', title: 'Hotels'},
+                {url: 'https://jobs.serandives.com', title: 'Jobs'},
+                {url: 'https://states.serandives.com', title: 'Real States'}
+            ]
+        }
+    ];
+    var menu = menus[0];
+    if (!menu) {
+        res.send(404, {
+            error: 'specified menu cannot be found'
+        });
+        return;
+    }
+    var token = req.token;
+    if (!token) {
+        res.send(menu);
+        return;
+    }
+    debug('auth token %s found for user %s', token.id, token.user);
+    menu.menu.push({
+        url: '/user',
+        title: 'Account'
+    });
+    res.send(menu);
+});
